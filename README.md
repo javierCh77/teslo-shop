7d6391dfffa878a8d1d3da9533d9e6a0fae62063
javierCh77 <javierchavarria21@gmail.com>
2024-05-09 11:31:07 -0300

    update

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 ## Run Dev mode
 
 1. Clone repository
-2. Install dependencies ```npm install```
-3. Run project  ```npm run dev ```
+2. Clone a copy ````.env.template``` and rename  ```-env``` and change enviroments
+3. Install dependencies ```npm install```
+4. UP data base ``` docker compose up -d```
+5. Run migrate Prisma ```npx prisma migrate dev ```
+6. Execute Seed ``` npm run seed```
+7. Run project  ```npm run dev ```
 
 ## Run Prod mode
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,27 @@
-import type { Metadata } from "next";
+
+
+import type { Metadata } from 'next';
 import { inter } from '@/config/fonts';
-import "./globals.css";
 
 
 
+import './globals.css';
 
 
 
 export const metadata: Metadata = {
-  title: "Teslo | shop",
-  description: "Una tienda virtual de productos",
-};
-
-
-
-
-
+  title: 'Teslo | Shop',
+  description: 'Una tienda virtual de productos',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
