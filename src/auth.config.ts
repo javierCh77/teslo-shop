c80bb9539b19f5e055ca05d4eb15fdcd2cb9bce1
javierCh77 <javierchavarria21@gmail.com>
2024-05-15 20:04:24 -0300

    login form status

---
+++ src/auth.config.ts
@@ -0,0 +1,56 @@
+import type { NextAuthConfig } from 'next-auth';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import prisma from '@/lib/prisma';
+import bcryptjs from 'bcryptjs';
+ 
+ 
+ 
+ 
+export const authConfig:NextAuthConfig = {
+  pages: {
+    signIn: '/auth/login',
+    newUser:'/auth/new-account',
+  },
+  providers:[
+    Credentials({
+      async authorize(credentials) {
+      
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+          
+          console.log(parsedCredentials.success)
+          
+          if( !parsedCredentials.success) return null;
+          
+          const { email, password} = parsedCredentials.data;    
+       
+          //search mail
+          const user= await prisma.user.findUnique({where: {email:email.toLocaleLowerCase()}});
+          if(!user) return null;
+          
+          //compara la contraseña
+          if(!bcryptjs.compareSync(password, user.password ) )  return null;
+          
+          // regreso el resto de los datos sin el password
+          const { password:_, ...rest} = user;
+          
+          console.log(rest)
+          return rest;
+          
+          console.log(rest);
+          
+          
+          
+        
+      },
+    }),
+    
+    
+  ]
+};
+
+
+export const { signIn, signOut, auth} = NextAuth( authConfig);
